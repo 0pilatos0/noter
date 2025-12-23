c4d3840f3f71a5f56e6c7c03ad9e7de0773e0610
Paul van der Lei <paul.vanderlei@davanti-wics.com>
2025-12-23 10:51:42 +0100

    Initial Commit

---
+++ noter/noterApp.swift
@@ -0,0 +1,17 @@
+//
+//  noterApp.swift
+//  noter
+//
+//  Created by Paul van der Lei on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct noterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
